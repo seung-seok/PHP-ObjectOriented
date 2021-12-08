e96cf00799557ed31c277223bf9e38a8f6b38c86
lee seungseok <new18@ggook.com>
2021-12-08 17:57:31 +0900

    211208

--- index.php
+++ index.php
@@ -1,1 +1,18 @@
-hi~
+<?php
+
+// 상태 : Property
+// 행동 : Methods
+// ->  : 접근 연산자
+class A
+{
+    public $message = 'hello, world';
+
+    public function foo()
+    {
+        return $this->message;
+    }
+}
+
+// class를 기반으로 객체 생성
+$a = new A(); 
+var_dump($a->foo());
